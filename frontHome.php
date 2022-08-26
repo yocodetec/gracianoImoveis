59ef6d4942c0db0df0294ba5e6af8577e4e82448
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-08-25 22:27:08 -0300

    Commit 1

---
+++ frontHome.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    		<meta charset="UTF-8">
+			
+      <!-- CSS -->
+			<link href="css/style.css" type="text/css" rel="stylesheet">
+      <!-- JQUERY -->
+			<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	  <!-- BOOTSTRAP -->
+	  		<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	</head>	
+	
+      <!-- CONTEÚDO -->
+	<body>
+	  <!-- MENU -->
+    	<div id="principal">
+        	<div class="col-lg-">
+              <div class="row">
+    			<div id="header">
+                    <nav class="navbar navbar-expand-lg fixed-top bg-white">
+                      <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                         <img src="img/logo_graciano.png" alt="" width="170" height="70" class="d-inline-block align-text-top">
+                         	
+                          </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse container-fluid" id="navbarNavDropdown">
+                          <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Buscar Imóveis
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Imóveis para alugar</a></li>
+                                <li><a class="dropdown-item" href="#">Imóveis para comprar</a></li>
+                                <li><a class="dropdown-item" href="#">Como comprar</a></li>
+                              </ul>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active" href="#">Sobre nós</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Serviços 
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Encontramos para você</a></li>
+                                <li><a class="dropdown-item" href="#">Simuladores</a></li>
+                                <li><a class="dropdown-item" href="#">Como vender</a></li>
+                              </ul>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active" href="#">Contato</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </nav>    				
+    			</div>
+    		</div>
+    			
+        	<div class="col-sm- div1">
+            <div class="col-sm- div2">
+                <br/>
+                <div class="titulos">
+                    NOSSOS SERVIÇOS
+                </div>
+
+                <div class="textos">
+					Somos uma empresa de desenvolvimento Web, e estamos prontas para tirar a sua idéia do papel <br/>
+					com nosso design para páginas web e nossos serviços de Desenvolvimento Back-end! <br/>
+					Também estamos dispostas a dar suporte técnico para o seu negócio!<br/>
+					Bem-Vindo(a) a nossa empresa YO.CODE Tec!
+                </div>
+            </div>
+            
+  			<div class="col-sm- div3"></div>
+  			<div class="col-sm- div4">
+                aaaaaaaaaaaaaaaaaa
+            </div>
+            <br/>
+			<div class="col-sm- div5">
+            </div>
+            <div class="col-sm- div6">
+            	bbbbbbbbbbbbbbbbbb
+            </div>
+  			<div class="col-sm- div7"></div>
+  			<div class="col-sm- div8">
+            	
+            	<div class="row row-cols-1 row-cols-md-3 g-4">
+                  <div class="col">
+                    <div class="card h-100">
+                      <img src="img/logo_graciano.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card h-100">
+                      <img src="img/logo_graciano.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a short card.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card h-100">
+                      <img src="img/logo_graciano.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  </div>
+                </div>
+                
+            </div>
+  
+  
+   </div> 			
+    					
+    			
+    	</div>
+    			<div id="footer">
+    				<div class="textoarea">
+    			   Telefone: 4002-8922 | Email: yo.codetec@gmail.com.br
+    				</div>
+    			</div>
+    	
+	</body>
+</html>     
