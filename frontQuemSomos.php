238d4901db9312b1df2bbee185a9c12366e092c0
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-09-15 15:00:55 -0300

    Update frontQuemSomos.php

--- frontQuemSomos.php
+++ frontQuemSomos.php
@@ -23,7 +23,7 @@
     			<div id="header">
                     <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
                       <div class="container-fluid">
-                        <a class="navbar-brand" href="frontHome.php">
+                        <a class="navbar-brand" href="index.php">
                         <!-- LOGO DA EMPRESA -->
                          <img src="img/logo_graciano.png"  alt="" width="170" height="70" class="d-inline-block align-text-top">
                           </a>
