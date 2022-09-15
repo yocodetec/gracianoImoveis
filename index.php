d7cd8fa93855220447d82460c7b367a8c4c5504a
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-09-15 14:51:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                               </ul>
                             </li>
                             <li class="nav-item">
-                              <a class="nav-link active" href="#">Sobre nós</a>
+                              <a class="nav-link active" href="frontQuemSomos.php">Sobre nós</a>
                             </li>
                             <li class="nav-item dropdown">
                               <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
