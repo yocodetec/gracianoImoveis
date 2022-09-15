4c702ef138c05b90c5b400aaf28c4afc198820bc
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-09-15 14:32:25 -0300

    Quem somos 15/09

---
+++ frontQuemSomos.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    		<meta charset="UTF-8">
+			
+      <!-- CSS -->
+			<link href="css/style2.css" type="text/css" rel="stylesheet">
+      <!-- JQUERY -->
+			<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	  <!-- BOOTSTRAP -->
+	  		<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	</head>	
+	
+      <!-- CONTEÚDO -->
+	<body>
+	  <!-- MENU -->
+        	<div class="col-lg-">
+              <div class="row">
+              <!-- CABEÇALHO -->
+    			<div id="header">
+                    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+                      <div class="container-fluid">
+                        <a class="navbar-brand" href="frontHome.php">
+                        <!-- LOGO DA EMPRESA -->
+                         <img src="img/logo_graciano.png"  alt="" width="170" height="70" class="d-inline-block align-text-top">
+                          </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <!-- BOTÕES DO CABEÇALHO -->
+                        <div class="collapse navbar-collapse container-fluid" id="navbarNavDropdown">
+                          <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Buscar Imóveis
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Imóveis para alugar</a></li>
+                                <li><a class="dropdown-item" href="#">Imóveis para comprar</a></li>
+                                <li><a class="dropdown-item" href="#">Como comprar</a></li>
+                              </ul>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active" href="frontQuemSomos.php">Sobre nós</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Serviços 
+                              </a>
+                              <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Encontramos para você</a></li>
+                                <li><a class="dropdown-item" href="#">Simuladores</a></li>
+                                <li><a class="dropdown-item" href="#">Como vender</a></li>
+                              </ul>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link active" href="loginyocode.php">Login</a>
+                            </li>
+                            
+                            
+                          </ul>
+                        </div>
+                      </div>
+                    </nav>    				
+    			</div>
+    		</div>
+    			
+            	<!-- PRIMEIRA DIV -->
+                <div class="col-sm- div2">
+                <!-- COLOCAR TEXTO --> 
+
+                </div>
+                
+                <div class="col-sm- div3">
+                <!-- DIV CINZA -->    
+                	<div class="col-sm- div4">	
+                		<div class="col-sm- div4-1">	
+                			<div class="col-sm- div4-2">	
+                	
+                			</div>
+                		</div>
+                	</div>
+                </div>
+                
+      			<div class="col-sm- div5">
+      			<!-- DIV AMARELA --> 
+      				<div class="col-sm- div6">
+      				<!-- DIV BRANCA -->
+                    	<div class="col-sm- div7">
+                    	<!-- DIV CINZA MENOR -->
+                    	</div>
+                    </div>	
+                    <div class="col-sm- div8"> 
+                    <!-- DIV BRANCA -->
+                    	<div class="col-sm- div9">
+    					<!-- DIV CINZA MENOR -->
+                    	</div>	
+                   </div>	
+      			</div>
+      			
+      			<div class="col-sm- div10">
+                <!-- DIV AZUL -->     
+                	<div class="col-sm- div11">
+    				<!-- DIV BRANCA 1 -->
+                    </div>	
+                    <div class="col-sm- div12">
+    				<!-- DIV BRANCA 2 -->
+                    </div>
+    			</div>
+
+ 		 <div id="footer">
+ 		 <!--  RODAPÉ  -->
+    		<div class="textoarea">
+    		   
+    		</div>
+    	</div>
+    	
+  		
+  </div>  			
+	</body>
+</html>  
