b1d7b62b16b9bba2b950871ada7e945b512389c7
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-11-03 21:46:58 -0300

    Update login.php
    03/11

--- login.php
+++ login.php
@@ -4,106 +4,57 @@
     		<meta charset="UTF-8">
 			
       <!-- CSS -->
-			<link href="css/login.css" type="text/css" rel="stylesheet">
+			<link href="css/teste.css" type="text/css" rel="stylesheet">
+			<link href="css/footer.css" type="text/css" rel="stylesheet">
       <!-- JQUERY -->
 			<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	  <!-- BOOTSTRAP -->
+	  		      <!-- CONFIGURAÇÃO DE MEDIA QUERY -->
 	  		<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	  <!-- ICONS -->
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 	</head>	
 	
       <!-- CONTEÚDO -->
 	<body>
-	  <!-- MENU -->
-        	<div class="col-lg-" id="toda">
-              <div class="row">
-              <!-- CABEÇALHO -->
-    			<div id="header">
-                    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-white">
-                      <div class="container-fluid">
-                        <a class="navbar-brand" href="index.php">
-                        <!-- LOGO DA EMPRESA -->
-                         <img src="img/logo_graciano.png" alt="" width="170" height="70" class="d-inline-block align-text-top">
-                          </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <!-- BOTÕES DO CABEÇALHO -->
-                        <div class="collapse navbar-collapse container-fluid" id="navbarNavDropdown">
-                          <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Buscar Imóveis
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Imóveis para alugar</a></li>
-                                <li><a class="dropdown-item" href="#">Imóveis para comprar</a></li>
-                                <li><a class="dropdown-item" href="#">Como comprar</a></li>
-                              </ul>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link active" href="frontQuemSomos.php">Sobre nós</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Serviços 
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Encontramos para você</a></li>
-                                <li><a class="dropdown-item" href="#">Simuladores</a></li>
-                                <li><a class="dropdown-item" href="#">Como vender</a></li>
-                              </ul>
-                            </li>
-                             <li class="nav-item">
-                              <a class="nav-link active" href="login.php">Login</a>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                    </nav>    				
-    			</div>
-    		</div><br>
+		<div class="container-fluid">
+			<div class="row">
+    		 <!-- MENU -->
+    		 <header id="header">
+    		     
+             </header>
+		 	 </div>
     			
-        	<!-- PRIMEIRA DIV -->
-           <div class="col-sm-" id="div2">
-           <br><br><br><br><br><br><br>
-           <div class="container">
-    		<div class="row">
-                 <div class="col-sm-4" id="div3">
-                 <form>
-                                  <div class="form-group">
-                                   <br><br>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" ex: yocodetec@gmail.com"><br>
-                                  </div>
-                                  <div class="form-group">
-                                
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="123">
-                                  </div>
-                                  <br>
-                                  <button type="submit" class="btn btn-primary">Submit</button>
-                  </form>
-                            	
-                 </div>
-            </div>
-            </div>
-            </div>
-  			   <!--  RODAPÉ  -->
-  			   
- 		
+    			<div class="row">
+                <div id="area-login">
+                    <img alt="" src="img/fundo-login.jpg" width="100%" height="100%" class="img-fundo darkened-image"/>
+                        <div class="card-login">
+                        <div id="titulo">
+                        	<h2>Login</h2>
+                        </div>
+                        
+                        <form action="" method="post">
+                            <div class="textfield">
+                            	<label for="usuario">Usuário</label>
+                            	<input type="text" name="nome" placeholder="Usuário" autofocus>
+                            </div>
+                            <div class="textfield">
+        						<label for="usuario">Senha</label>
+        						<input type="password" name="senha" placeholder="Senha">
+                        	</div>
+                        	
+                        	<button class="btn-login">Entrar</button>
+                        </form>
+                        <p>Ainda não tem uma conta?<a href="#">Criar conta</a></p>
+                        	
+                        </div>
+                        
+        		</div>
+    			</div>
+    	
+  		</div>
 	</body>
-	<footer class="container-fluid text-center" id="footer">
-  
-  
-  
-			<p style="font-size: 20px; font-family: Arial; color: #fff;">
-			&copy; Todos os direitos reservados</p>
-                 </div> 
-          </div>
-      </div>
-</footer>
-	
-	
-	
-</html>     
+</html>
