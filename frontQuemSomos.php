7e31dc7ffb9fbe6f2c927b2ef4a0930c9260f302
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-11-03 22:20:23 -0300

    Update frontQuemSomos.php

--- frontQuemSomos.php
+++ frontQuemSomos.php
@@ -1,136 +1,135 @@
-<!DOCTYPE html>
-<html>
-	<head>
-    		<meta charset="UTF-8">
+<!DOCTYPEhtml>
+< html >
+	< cabeça >
+    		< meta  charset = " UTF-8 " >
 			
       <!-- CSS -->
-			<link href="css/style2.css" type="text/css" rel="stylesheet">
-			<link href="css/header.css" type="text/css" rel="stylesheet">
-			<link href="css/footer.css" type="text/css" rel="stylesheet">
+			< link  href = " css/style2.css "  type = " text/css "  rel = " stylesheet " >
+			< link  href = " css/header.css "  type = " text/css "  rel = " stylesheet " >
+			< link  href = " css/footer.css "  type = " text/css "  rel = " stylesheet " >
       <!-- JQUERY -->
-			<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+			< script  src = " https://code.jquery.com/jquery-3.6.0.min.js "  integridade = " sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4= "  crossorigin = " anônimo " ></ script >
 	  <!-- BOOTSTRAP -->
 	  		      <!-- CONFIGURAÇÃO DE MEDIA QUERY -->
-	  		<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-	  <!-- ICONS -->
-			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-	</head>	
+	  		< meta  name = " viewport "  content = " largura = largura do dispositivo, escala inicial = 1 " >
+			< link  href = " https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css "  rel = " stylesheet " >
+			< script  src = " https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js " ></ script >
+			< link  rel = " stylesheet "  href = " https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css " >
+	  <!-- ÍCONES -->
+			< link  rel = " stylesheet "  href = " https://fonts.googleapis.com/css2?family=Material+Symbols+ Outlined:opsz,wght,FILL,GRAD@48,400,0,0 " />
+	</ cabeça >	
 	
       <!-- CONTEÚDO -->
-	<body>
-		<div class="container-fluid">
-			<div class="row">
+	< corpo >
+		< classe div  = " contêiner-fluido " >
+			< classe div  = " linha " >
     		 <!-- MENU -->
-    		 <header id="header">
+    		 < id do cabeçalho  = " cabeçalho " >
     		 
-        		 <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-white border-bottom">
-                      <div class="container-fluid">
-                        <a class="navbar-brand" href="frontHome.php">
+        		 < nav  class = " navbar navbar-expand-lg fixed-top navbar-light bg-white border-bottom " >
+                      < classe div  = " contêiner-fluido " >
+                        < a  class = " navbar-brand "  href = " frontHome.php " >
                         	 <!-- LOGO DA EMPRESA -->
-                             <img src="" alt="" width="" height="" class="d-inline-block align-text-top">
-                        </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="navbar-toggler-icon"></span>
-                        </button>
+                             < img  src = " "  alt = " "  largura = " "  altura = " "  class = " d-inline-block align-text-top " >
+                        </a> _ _
+                        < button  class = " navbar-toggler "  type = " button "  data-bs-toggle = " recolher "  data-bs-target = " #navbarSupportedContent "  aria-controls = " navbarSupportedContent "  aria-expanded = " false "  aria-label = " Alternar navegação " >
+                          < span  class = " navbar-toggler-icon " ></ span >
+                        </ botão >
                         <!-- BOTÕES DO MENU -->
-                        <ul class="navbar-nav">
- 						   <li class="nav-item dropdown">
-                              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        < ul  class = " navbar-nav " >
+ 						   < li  class = " menu suspenso do item de navegação " >
+                              < a  class = " nav-link dropdown-toggle "  href = " # "  role = " button "  data-bs-toggle = " dropdown "  aria-expanded = " false " >
                                 Buscar imóveis
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Imóveis para alugar</a></li>
-                                <li><a class="dropdown-item" href="#">Imóveis para comprar</a></li>
-                                <li><a class="dropdown-item" href="#">Como comprar</a></li>
-                              </ul>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="#">Sobre nós</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              </a> _ _
+                              < ul  class = " menu suspenso " >
+                                < li >< a  class = " dropdown-item "  href = " # " >Imóveis para alugar</ a ></ li >
+                                < li >< a  class = " dropdown-item "  href = " # " >Imóveis para comprar</ a ></ li >
+                                < li >< a  class = " dropdown-item "  href = " # " >Como comprar</ a ></ li >
+                              </ ul >
+                            </ li >
+                            < li  class = " nav-item " >
+                              < a  class = " nav-link "  href = " # " >Sobre nós</ a >
+                            </ li >
+                            < li  class = " menu suspenso do item de navegação " >
+                              < a  class = " nav-link dropdown-toggle "  href = " # "  role = " button "  data-bs-toggle = " dropdown "  aria-expanded = " false " >
                                 Serviços
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Encontramos para você</a></li>
-                                <li><a class="dropdown-item" href="#">Simuladores</a></li>
-                                <li><a class="dropdown-item" href="#">Como vender</a></li>
-                              </ul>
-                           </li>
-					  </ul>
-                          <form class="d-flex">
-                            <button class="btn btn-outline-secondary" type="submit">Login</button>
-                          </form>
+                              </a> _ _
+                              < ul  class = " menu suspenso " >
+                                < li >< a  class = " dropdown-item "  href = " # " >Encontramos para você</ a ></ li >
+                                < li >< a  class = " dropdown-item "  href = " # " > Simuladores</ a ></ li >
+                                < li >< a  class = " dropdown-item "  href = " # " >Como vender</ a ></ li >
+                              </ ul >
+                           </ li >
+					  </ ul >
+                          < form  class = " d-flex " >
+                            < button  class = " btn btn-outline-secondary "  type = " submit " >Login</ button >
+                          </ formulário >
                         
-                      </div>
-                 </nav>
+                      </ div >
+                 </ nav >
     
-             </header>
-		 	 </div>
+             </ cabeçalho >
+		 	 </ div >
     			
-    			<div class="row">
+    			< classe div  = " linha " >
             	<!-- PRIMEIRA DIV -->
-                <div class="col-sm- div2">
+                < div  class = " col-sm- "  id = " div2 " >
                 <!-- COLOCAR TEXTO --> 
-
-                </div>
-                </div>
+                </ div >
+                </ div >
                 
-				<div class="row">
-                <div class="col-sm- div3">
+				< classe div  = " linha " >
+                < div  class = " col-sm- "  id = " div3 " >
                 <!-- DIV CINZA -->    
-                	<div class="col-sm- div4">	
-                		<div class="col-sm- div4-1">	
-                			<div class="col-sm- div4-2">	
+                	< div  class = " col-sm- "  id = " div4 " >	
+                		< div  class = " col-sm- "  id = " div4-1 " >	
+                			< div  class = " col-sm- "  id = " div4-2 " >	
                 	
-                			</div>
-                		</div>
-                	</div>
-                </div>
-                </div>
+                			</ div >
+                		</ div >
+                	</ div >
+                </ div >
+                </ div >
                 
-				<div class="row">
-      			<div class="col-sm- div5">
+				< classe div  = " linha " >
+      			< div  class = " col-sm- "  id = " div5 " >
       			<!-- DIV AMARELA --> 
-      				<div class="col-sm- div6">
+      				< div  class = " col-sm- "  id = " div6 " >
       				<!-- DIV BRANCA -->
-                    	<div class="col-sm- div7">
+                    	< div  class = " col-sm- "  id = " div7 " >
                     	<!-- DIV CINZA MENOR -->
-                    	</div>
-                    </div>	
-                    <div class="col-sm- div8"> 
+                    	</ div >
+                    </ div >	
+                    < div  class = " col-sm- "   id = " div8 " >
                     <!-- DIV BRANCA -->
-                    	<div class="col-sm- div9">
+                    	< div  class = " col-sm- "  id = " div9 " >
     					<!-- DIV CINZA MENOR -->
-                    	</div>	
-                   </div>	
-      			</div>
-      			</div>
+                    	</ div >	
+                   </ div >	
+      			</ div >
+      			</ div >
       			
-  				<div class="row">
-      			<div class="col-sm- div10">
+  				< classe div  = " linha " >
+      			< div  class = " col-sm- "  id = " div10 " >
                 <!-- DIV AZUL -->     
-                	<div class="col-sm- div11">
+                	< div  class = " col-sm- "  id = " div11 " >
     				<!-- DIV BRANCA 1 -->
-                    </div>	
-                    <div class="col-sm- div12">
+                    </ div >	
+                    < div  class = " col-sm- "  id = " div12 " >
     				<!-- DIV BRANCA 2 -->
-                    </div>
-    			</div>
-    			</div>
+                    </ div >
+    			</ div >
+    			</ div >
     			
-    	<div class="row">
- 		 <div id="footer">
- 		 <!--  RODAPÉ  -->
-    		<div class="textoarea">
+    	< classe div  = " linha " >
+ 		 < div  id = " rodapé " >
+ 		 <!--   RODAPÉ   -->
+    		< classe div  = " textarea " >
     		   
-    		</div>
-    	</div>
-    	</div>
+    		</ div >
+    	</ div >
+    	</ div >
     	
-  		</div>
-	</body>
-</html>	
+  		</ div >
+	</ corpo >
+</ html > 
