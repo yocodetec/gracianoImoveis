f2c8ddb7d6df170037dbd97374579159997d8e1c
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-12-01 20:22:13 -0300

    Update and rename login/parabens.php to Conecte-se/parabens.php

--- Conecte-se/parabens.php
+++ Conecte-se/parabens.php
@@ -1,10 +1,10 @@
 <?php 
 session_start();
-if ( (isset($_SESSION['email']) && isset($_SESSION['senha']))){
+if ( ! (isset($_SESSION['email']) && isset($_SESSION['senha']))){
 echo "";
 
 }else {
-	header("Location: ./login.php");
+	header("Location: ../login.php");
 
 }
 	?> 
