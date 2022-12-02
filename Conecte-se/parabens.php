2a42d9c7813d56f7f291af526a2109851b33f9e0
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-12-02 12:41:40 -0300

    Create parabens.php

---
+++ Conecte-se/parabens.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+if ( (isset($_SESSION['email']) && isset($_SESSION['senha']))){
+echo "";
+
+}else {
+	header("Location: ../login.php");
+
+}
+	?> 
+	<html>
+	<form action="logoff.php" method="post">
+					 
+			
+					 <button type="submit" class="btn btn-primary">Sair</button>
+	 </form>
+	</html>
