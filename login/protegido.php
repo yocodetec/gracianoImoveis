b40a105ec3a3a71db70648c0a78f2345d3eb2690
yocodetec <104228157+yocodetec@users.noreply.github.com>
2022-12-02 12:57:20 -0300

    Add files via upload

---
+++ login/protegido.php
@@ -0,0 +1,20 @@
+
+<?php
+    session_start();
+    if ((isset($_SESSION['email']) && isset($_SESSION['senha']))){
+       header("Location: ../login.php");
+
+    if (isset($_SESSION['idUsuario']) == 7){
+        header("Location: ./protegidoADM.php");
+    }else{ 
+    
+        header("Location: ./parabens.php");}
+    
+    
+    
+    
+    }
+
+    
+?>
+
